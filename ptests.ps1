66336c8bb32cf9ac5a210aa5a22e2e694a3305c9
RamyaBellamkonda <129672695+RamyaBellamkonda@users.noreply.github.com>
2023-08-30 14:20:55 +0100

    Update ptests.ps1

--- ptests.ps1
+++ ptests.ps1
@@ -2,13 +2,13 @@ Describe 'Static Website' {
 
   context 'When the website is deployed' {
     It 'should have the correct content' {
-      $content = Invoke-WebRequest 'https://mycdnendpoint-green.azureedge.net'
+      $content = Invoke-WebRequest 'https://mycdnendpoint-green.azureedge.net/'
       $content  = $response.Content
       $content | Should -Be 'This is my static website!'
     }
 
     It 'should be accessible over HTTPS' {
-      $response = Invoke-WebRequest 'https://mycdnendpoint-green.azureedge.net'
+      $response = Invoke-WebRequest 'https://mycdnendpoint-green.azureedge.net/'
       $response.StatusCode | Should -Be 200
     }
   }
